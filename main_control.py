b5b8df2b34f1377ce9778d607b9b1ea4a27bf6e6
Avinier Nakamoto <adisubu.2410@gmail.com>
2021-10-02 22:08:59 +0530

    fixed button implemention bug

--- main_control.py
+++ main_control.py
@@ -1,5 +1,4 @@
-# TODO: Make GUI with Tkinter (Aditya- Developing, Dhruv- Designing)
-#TODO: Fix minor GUI bugs
+# TODO: Beautify GUI
 # TODO: Make exe file (app)
 # TODO: Fine tune code (later)
 # TODO: Fix volume-control bug (later)
@@ -9,32 +8,48 @@ import time
 import hand_tracking_module as htm
 import volume_control_module as vsc
 import gesture_controls_module as gsc
-# import control_gui as gui
 import rps_module as rps
-
-from tkinter import *
-from PIL import Image, ImageTk
-
-# Create an instance of TKinter Window or frame
-win = Tk()
-# Set the size of the window
-win.geometry("700x350")
-# Create a Label to capture the Video frames
-label =Label(win)
-label.grid(row=0, column=0)
+# import control_gui as gui
 
 vid = cv.VideoCapture(0)
 width_cam, height_cam = 1000, 620
 vid.set(3, width_cam)
 vid.set(4, height_cam)
 
-# gui.vol_toggle()
+# img = None
+# lm_list = []
+
+vol_flag = False
+gesture_flag = False
+rps_flag = False
+
+def set_volflag(flag_bool):
+    global vol_flag
+    vol_flag = flag_bool
+
+def get_volflag():
+    return vol_flag
+
+def set_gestureflag(gestureflag):
+    global gesture_flag
+    gesture_flag = gestureflag
+
+def get_gestureflag():
+    return gesture_flag
+
+def set_rpsflag(rpsflag):
+    global rps_flag
+    rps_flag = rpsflag
+
+def get_rpsflag():
+    return rps_flag
+
 def main():
     detector = htm.HandDetector()
     prev_time = 0
-    vol_flag = False
-    gesture_flag = False
-    rps_flag = False
+
+    global vol_flag, gesture_flag, rps_flag
+    # global img, lm_list
     while True:
         success, img = vid.read()
         img = detector.detect_hands(img)
@@ -55,9 +70,9 @@ def main():
         cv.waitKey(1)
         cv.imshow("Volume Control", img)
 
-main()
+        cv.waitKey(1)
 
-
-
-
-
+        if cv.waitKey(1) & 0xFF == ord("q"):
+            break
+            # vid.release()
+            # cv.destroyAllWindows()
